00833b06698c0f188541cbbaf4bf830409fa381a
travis <travislbrundage@gmail.com>
2017-01-09 13:34:26 -0800

    Force usernames lowercase

--- exchange/auth/middleware.py
+++ exchange/auth/middleware.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.middleware import RemoteUserMiddleware
+from django.contrib import auth
 
 
 class GeoAxisMiddleware(RemoteUserMiddleware):
@@ -10,3 +11,16 @@ class GeoAxisMiddleware(RemoteUserMiddleware):
     """
     header = getattr(settings, 'GEOAXIS_HEADER', 'OAM_REMOTE_USER')
     force_logout_if_no_header = False
+
+    def clean_username(self, username, request):
+        """
+        Allows the backend to clean the username, if the backend defines a
+        clean_username method.
+        """
+        backend_str = request.session[auth.BACKEND_SESSION_KEY]
+        backend = auth.load_backend(backend_str)
+        try:
+            username = backend.clean_username(username)
+        except AttributeError:
+            pass
+        return username.lower()
