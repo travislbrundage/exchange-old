04fea6fba91bad5d5ce4d0a0994c0dc325135ef7
Alexis Rhote <arhote@boundlessgeo.com>
2018-01-25 10:54:51 -0800

    Improve comments and appease linter formatting (#550)
    * Improve comments and appease linter formatting
    
    * Appease the linter

--- exchange/pki/settings.py
+++ exchange/pki/settings.py
@@ -2,16 +2,29 @@ import os
 import ssl
 
 '''
-name: string (display name of config, shown in popup on remote services registration page and details page after registration)
-ca_custom_certs: filesystem path; default None
-ca_use_system: bool; default True (if True, any custom certs are appended)
-ca_allow_invalid_certs: bool; default False
-ca_skip_peer_validation: bool; default False
-client_cert: filesystem path; default None
-client_key: filesystem path; default None
-client_key_pass: string; default None
-ssl_allow_insecure: bool; default False
-ssl_version: string; default {{ssl.PROTOCOL_SSLv23}} (from standard Python {{ssl}} package)
+name:                       string (display name of config, shown in popup on
+                            remote services registration page and details page
+                            after registration)
+
+ca_custom_certs:            filesystem path; default None
+
+ca_use_system:              bool; default True (if True, any custom certs are
+                            appended)
+
+ca_allow_invalid_certs:     bool; default False
+
+ca_skip_peer_validation:    bool; default False
+
+client_cert:                filesystem path; default None
+
+client_key:                 filesystem path; default None
+
+client_key_pass:            string; default None
+
+ssl_allow_insecure:         bool; default False
+
+ssl_version:                string; default {{ssl.PROTOCOL_SSLv23}} (from
+                            standard Python {{ssl}} package)
 '''
 SSL_DEFAULT_CONFIG = {
     "name": "Default",
@@ -28,3 +41,28 @@ SSL_DEFAULT_CONFIG = {
 
 SSL_CONFIGS = [SSL_DEFAULT_CONFIG]
 
+'''
+Example for adding new configs beyond the default
+
+SSL_CONFIGS = [
+    {
+        "name": "Some high-security network",
+        "ca_custom_certs": "/path/to/ca_certs.crt",
+        "ssl_version": ssl.PROTOCOL_TLSv1_2
+    },
+    {
+        "name": "Some high-security network with PKI",
+        "ca_custom_certs": "/path/to/ca_certs.crt",
+        "client_cert": "/path/to/npe_cert.crt",
+        "client_key": "/path/to/npe.key",
+        "client_key_pass": "password",
+        "ssl_version": ssl.PROTOCOL_TLSv1_2
+    }
+]
+
+Note: It is NOT required to implement all settings in new configs, any values
+that are excluded will assume the values of SSL_DEFAULT_CONFIG
+
+Note: If you add additional configs, you should not include SSL_DEFAULT_CONFIG
+in your SSL_CONFIGS list.
+'''
