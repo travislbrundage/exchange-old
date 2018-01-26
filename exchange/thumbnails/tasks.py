b3f06c83587a3529103915f1b98ddfeb080508dc
David Bitner <bitner@dbspatial.com>
2018-01-26 11:33:10 -0600

    Rework thumbs to use requests bex-660 (#551)

--- exchange/thumbnails/tasks.py
+++ exchange/thumbnails/tasks.py
@@ -1,18 +1,75 @@
-import time
 import logging
-import base64
+import time
 from celery.task import task
 
 from django.db.models.signals import post_save
 from django.conf import settings
 from geonode.layers.models import Layer
 from geonode.maps.models import Map
-from geonode.utils import http_client
-
+from geonode.geoserver.helpers import ogc_server_settings
+from requests import session
+from requests.adapters import HTTPAdapter, Retry
 from .models import is_automatic
 from .models import save_thumbnail
 
 logger = logging.getLogger(__name__)
+
+# setup requests client with max retries
+http_client = session()
+http_client.verify = True
+retry = Retry(
+    total=4,
+    status=4,
+    backoff_factor=0.9,
+    status_forcelist=[502, 503, 504],
+    method_whitelist=set([
+        'HEAD',
+        'TRACE',
+        'GET',
+        'PUT',
+        'POST',
+        'OPTIONS',
+        'DELETE'
+    ])
+)
+http_client.mount('http://', HTTPAdapter(max_retries=retry))
+http_client.mount('https://', HTTPAdapter(max_retries=retry))
+
+
+def make_thumb_request(instance, baseurl, params=None):
+    # Avoid using urllib.urlencode here because it breaks the url.
+    # commas and slashes in values get encoded and then cause trouble
+    # with the WMS parser.
+    if params:
+        p = "&".join("%s=%s" % item for item in params.items())
+    else:
+        p = ''
+
+    thumbnail_create_url = baseurl + p
+    logger.debug(
+        'Thumbnail: Requesting thumbnail for %s. ',
+        thumbnail_create_url
+    )
+    if (instance.storeType == 'remoteStore'):
+        resp = http_client.get(thumbnail_create_url)
+    else:
+        # Login using basic auth as geoserver admin
+        user = settings.GEOSERVER_USER
+        pword = settings.GEOSERVER_PASSWORD
+        resp = http_client.get(
+            thumbnail_create_url,
+            auth=(user, pword)
+        )
+    if 200 <= resp.status_code <= 299:
+        if 'ServiceException' not in resp.content:
+            return resp.content
+        else:
+            logger.debug(
+                'Thumbnail: Encountered unexpected status code: %d.  '
+                'Aborting.',
+                resp.status_code)
+            logger.debug(resp)
+    return None
 
 
 # Get a thumbnail image generated from GeoServer
@@ -23,7 +80,13 @@ logger = logging.getLogger(__name__)
 # @return PNG bytes.
 #
 def get_gs_thumbnail(instance):
-    from geonode.geoserver.helpers import ogc_server_settings
+    if (instance.storeType == 'remoteStore'):
+        if (instance.service.type == 'REST'):
+            thumbnail_create_url = "%s/info/thumbnail" % (instance.ows_url)
+            content = make_thumb_request(instance, thumbnail_create_url)
+            if content:
+                return content
+            return None
 
     if instance.class_name == 'Map':
         local_layers = []
@@ -42,84 +105,50 @@ def get_gs_thumbnail(instance):
         'format': 'image/png8',
         'width': 200,
         'height': 150,
-        'TIME': '-99999999999-01-01T00:00:00.0Z/99999999999-01-01T00:00:00.0Z'
+        'TIME': '-99999999999-01-01T00:00:00.0Z/99999999999-01-01T00:00:00.0Z',
+        'crs': 'EPSG:3857'
     }
 
     baseurl = ogc_server_settings.LOCATION + \
         "wms/reflect?"
 
     if (instance.storeType == 'remoteStore'):
-        params['request'] = 'getMap'
+        params['request'] = 'GetMap'
         params['service'] = 'wms'
         params['version'] = '1.3.0'
+        params['crs'] = 'EPSG:4326'
         params['bbox'] = '%s,%s,%s,%s' % (
-            instance.bbox_x0,
             instance.bbox_y0,
-            instance.bbox_x1,
-            instance.bbox_y1
+            instance.bbox_x0,
+            instance.bbox_y1,
+            instance.bbox_x1
         )
+        params['styles'] = ''
 
         baseurl = instance.ows_url + '?'
         del params['TIME']
 
-        if all(s in baseurl.lower() for s in ['mapserver', 'wmsserver']):
-            params['styles'] = ''
-            params['crs'] = 'epsg:4326'
+    content = make_thumb_request(instance, baseurl, params)
+    if content:
+        return content
 
-    # Avoid using urllib.urlencode here because it breaks the url.
-    # commas and slashes in values get encoded and then cause trouble
-    # with the WMS parser.
-    p = "&".join("%s=%s" % item for item in params.items())
+    # try using jpeg rather than png
+    params['format'] = 'image/jpeg'
+    content = make_thumb_request(instance, baseurl, params)
+    if content:
+        return content
 
-    thumbnail_create_url = baseurl + p
-
-    if (instance.storeType == 'remoteStore'):
-        if (instance.service.type == 'REST'):
-            thumbnail_create_url = "%s/info/thumbnail" % (instance.ows_url)
-
-    tries = 0
-    max_tries = 15
-    while tries < max_tries:
-        logger.debug(
-            'Thumbnail: Requesting thumbnail from GeoServer. '
-            'Attempt %d of %d for %s',
-            tries + 1, max_tries, thumbnail_create_url)
-        if (instance.storeType == 'remoteStore'):
-            if tries > 4:
-                thumbnail_create_url = thumbnail_create_url.replace(
-                    'image/png8', 'image/jpeg')
-            resp, image = http_client.request(thumbnail_create_url)
-        else:
-            # Login using basic auth as geoserver admin
-            user = settings.GEOSERVER_USER
-            pword = settings.GEOSERVER_PASSWORD
-            auth = base64.encodestring(user + ':' + pword)
-            resp, image = http_client.request(
-                thumbnail_create_url,
-                'GET',
-                headers={'Authorization': 'Basic ' + auth}
-            )
-        if 200 <= resp.status <= 299:
-            if 'ServiceException' not in image:
-                return image
-            else:
-                logger.debug(
-                    'Thumbnail: Encountered unexpected status code: %d.  '
-                    'Aborting.',
-                    resp.status)
-                logger.debug(resp)
-        else:
-            # Unexpected Error Code, Stop Trying
-            logger.debug(
-                'Thumbnail: Encountered unexpected status code: %d.  '
-                'Aborting.',
-                resp.status)
-            logger.debug(resp)
-            break
-
-        # Layer not ready yet, try again
-        tries += 1
-        time.sleep(3)
+    # try with CRS=84
+    params['crs'] = 'CRS:84'
+    params['bbox'] = '%s,%s,%s,%s' % (
+        instance.bbox_x0,
+        instance.bbox_y0,
+        instance.bbox_x1,
+        instance.bbox_y1
+    )
+    content = make_thumb_request(instance, baseurl, params)
+    if content:
+        return content
 
     return None
 
@@ -137,9 +166,20 @@ def generate_thumbnail_task(instance_id, class_name):
             # Instance not saved yet, nothing more we can do
             logger.debug(
                 'Thumbnail: Layer \'%s\' does not yet exist, cannot '
-                'generate thumbnail.',
+                'generate thumbnail. Trying one more time in 5 seconds.',
                 instance_id)
-            return
+            time.sleep(5)
+            try:
+                instance = Layer.objects.get(typename=instance_id)
+                obj_type = 'layers'
+            except Layer.DoesNotExist:
+                # Instance not saved yet, nothing more we can do
+                logger.debug(
+                    'Thumbnail: Layer \'%s\' does not yet exist, cannot '
+                    'generate thumbnail.',
+                    instance_id)
+                return
+
     elif class_name == 'Map':
         try:
             instance = Map.objects.get(id=instance_id)
@@ -148,9 +188,19 @@ def generate_thumbnail_task(instance_id, class_name):
             # Instance not saved yet, nothing more we can do
             logger.debug(
                 'Thumbnail: Map \'%s\' does not yet exist, cannot '
-                'generate thumbnail.',
+                'generate thumbnail. Trying one more time in 5 seconds.',
                 instance_id)
-            return
+            time.sleep(5)
+            try:
+                instance = Map.objects.get(id=instance_id)
+                obj_type = 'maps'
+            except Map.DoesNotExist:
+                # Instance not saved yet, nothing more we can do
+                logger.debug(
+                    'Thumbnail: Map \'%s\' does not yet exist, cannot '
+                    'generate thumbnail.',
+                    instance_id)
+                return
     else:
         logger.debug(
             'Thumbnail: Unsupported class: %s. Aborting.', class_name)
