851288861e4f657caf354b43fe20cfacd41d6224
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-01-29 09:03:32 -0700

    Update PKI settings to match urllib3 SSL context params; add config map

--- exchange/pki/settings.py
+++ exchange/pki/settings.py
@@ -1,68 +1,103 @@
 import os
-import ssl
 
 '''
-name:                       string (display name of config, shown in popup on
-                            remote services registration page and details page
-                            after registration)
+name:
+    string; REQUIRED (display name of config, shown in popup on remote services
+    registration page and details page after registration)
 
-ca_custom_certs:            filesystem path; default None
+ca_custom_certs:
+    None or filesystem path; default None.
+    If None, urllib3 defaults to None, and OpenSSL system CA certs are used.
 
-ca_use_system:              bool; default True (if True, any custom certs are
-                            appended)
+ca_allow_invalid_certs:
+    bool; default False. Used during validation of SSL components in a config.
 
-ca_allow_invalid_certs:     bool; default False
+client_cert:
+    None oir filesystem path; default None.
+    Path should be to a PEM file of client cert (with no private key included).
+    If None, urllib3 defaults to None.
 
-ca_skip_peer_validation:    bool; default False
+client_key:
+    None or filesystem path (REQUIRED, if client_cert defined); default None.
+    Path should be to a PEM file of client cert key
+    If None, urllib3 defaults to None.
 
-client_cert:                filesystem path; default None
+client_key_pass:
+    None or string; default None.
+    If None, urllib3 defaults to None.
 
-client_key:                 filesystem path; default None
+ssl_version:
+    None or string; default 'PROTOCOL_SSLv23'.
+    ('PROTOCOL_*' options from standard Python {{ssl}} package).
+    If None, urllib3 defaults to CERT_NONE (insecure).
 
-client_key_pass:            string; default None
+ssl_verify_mode:
+    None or string; default 'CERT_REQUIRED'.
+    ('CERT_*' options from standard Python {{ssl}} package).
+    If None, urllib3 defaults to CERT_NONE (insecure).
 
-ssl_allow_insecure:         bool; default False
+ssl_options:
+    None or list of strings; default None
+    ('OP_*' options from standard Python {{ssl}} package.)
+    If None, urllib3 defaults to a secure set of options for modern TLS:
+    ['OP_NO_SSLv2', 'OP_NO_SSLv3', 'OP_NO_COMPRESSION']
+    It is recommend to use these options, plus any you wish to add.
 
-ssl_version:                string; default {{ssl.PROTOCOL_SSLv23}} (from
-                            standard Python {{ssl}} package)
+ssl_ciphers:
+    None or string of OpenSSL ciphers.
+    See: https://wiki.openssl.org/index.php/Manual:Ciphers(1)
+         and (https://www.openssl.org/docs/man1.0.2/apps/ciphers.html
+              or https://www.openssl.org/docs/man1.1.0/apps/ciphers.html).
+    If None, urllib3 defaults to its DEFAULT_CIPHERS.
+
+https_retries:
+    None, int >= 0 or False.
+    (0 does not retry; False does the same, but skips rasising error.)
+
+https_redirects:
+    None or int >= 0 or False.
+    (0 doesn't follow redirect; False does the same, but skips rasising error.)
 '''
 SSL_DEFAULT_CONFIG = {
     "name": "Default",
     "ca_custom_certs": None,
-    "ca_use_system": True,
     "ca_allow_invalid_certs": False,
-    "ca_skip_peer_validation": False,
     "client_cert": None,
-    "client_key": None,
+    "client_key": None,  # TODO: Enforce separate client/key files in help text
     "client_key_pass": None,
-    "ssl_allow_insecure": False,
-    "ssl_version": ssl.PROTOCOL_SSLv23
+    "ssl_version": "PROTOCOL_SSLv23",
+    "ssl_verify_mode": "CERT_REQUIRED",
+    # "ssl_verify_flags": None,  # default flags are good enough
+    "ssl_options": None,
+    "ssl_ciphers": None,
+    "https_retries": 3,
+    "https_redirects": 3,
 }
 
-SSL_CONFIGS = [SSL_DEFAULT_CONFIG]
+SSL_CONFIGS = {}
 
 '''
 Example for adding new configs beyond the default
 
-SSL_CONFIGS = [
-    {
-        "name": "Some high-security network",
+SSL_CONFIGS = {
+    'config1': {
+        "name": "TLS v1.2-only network",
         "ca_custom_certs": "/path/to/ca_certs.crt",
         "ssl_version": ssl.PROTOCOL_TLSv1_2
     },
-    {
-        "name": "Some high-security network with PKI",
+    'config2': {
+        "name": "TLS v1.2-only network with PKI",
         "ca_custom_certs": "/path/to/ca_certs.crt",
         "client_cert": "/path/to/npe_cert.crt",
         "client_key": "/path/to/npe.key",
         "client_key_pass": "password",
         "ssl_version": ssl.PROTOCOL_TLSv1_2
     }
-]
+}
 
 Note: It is NOT required to implement all settings in new configs, any values
 that are excluded will assume the values of SSL_DEFAULT_CONFIG
-
-Note: If you add additional configs, you should not include SSL_DEFAULT_CONFIG
-in your SSL_CONFIGS list.
 '''
+
+# This TEMPORARILY represents an ssl_config_map model
+SSL_CONFIG_MAP = {}
