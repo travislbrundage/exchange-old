92bfb126116df7a144522f15fe29e48af059b587
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-02-09 10:02:19 -0700

    Turn off debug logging

--- exchange/settings/default.py
+++ exchange/settings/default.py
@@ -356,7 +356,7 @@ if AUDIT_ENABLED:
 # 'DEBUG', 'INFO', 'WARNING', 'ERROR', or 'CRITICAL'
 DJANGO_LOG_LEVEL = os.getenv('DJANGO_LOG_LEVEL', 'ERROR')
 # Commenting out logging until it can be modified to log errors correctly
-
+'''
 installed_apps_conf = {
     'handlers': ['console'],
     'level': DJANGO_LOG_LEVEL,
@@ -393,7 +393,7 @@ LOGGING['loggers']['django.db.backends'] = {
     'propagate': False,
     'level': 'WARNING',  # Django SQL logging is too noisy at DEBUG
 }
-
+'''
 # Authentication Settings
 
 # ldap
