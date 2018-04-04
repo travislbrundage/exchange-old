9144628ef2e9ba7616bded7f03a88cd32a4a3278
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-04-04 12:04:12 -0600

    Add unit tests for SslContextAdapter class

--- exchange/tests/pki_test.py
+++ exchange/tests/pki_test.py
@@ -26,7 +26,7 @@ import pytest
 import django
 
 from urllib import quote, quote_plus
-from requests import get
+from requests import get, Request
 from requests.exceptions import ConnectionError, SSLError
 
 from django.conf import settings
@@ -64,6 +64,7 @@ from exchange.pki.utils import (
     protocol_relative_to_scheme,
     relative_to_absolute_url,
     hostname_port,
+    normalize_hostname,
     requests_base_url,
     pki_prefix,
     pki_site_prefix,
@@ -155,6 +156,76 @@ class PkiTestCase(ExchangeTest):
             ptn, ssl_config)
         logger.debug("Hostname:Port mappings:\n{0}"
                      .format(HostnamePortSslConfig.objects.all()))
+
+
+class TestSslContextSessionAdapter(PkiTestCase):
+
+    def setUp(self):
+        HostnamePortSslConfig.objects.all().delete()
+        self.assertEqual(HostnamePortSslConfig.objects.count(), 0)
+
+        self.assertEqual(SslConfig.objects.count(), 8)
+        self.ssl_config_1 = SslConfig.objects.get(pk=1)
+        self.ssl_config_2 = SslConfig.objects.get(pk=2)
+        self.ssl_config_3 = SslConfig.objects.get(pk=3)
+        self.ssl_config_4 = SslConfig.objects.get(pk=4)   # PKI: key with pass
+        self.ssl_config_5 = SslConfig.objects.get(pk=5)
+        self.ssl_config_6 = SslConfig.objects.get(pk=6)
+        self.ssl_config_7 = SslConfig.objects.get(pk=7)
+        self.ssl_config_8 = SslConfig.objects.get(pk=8)
+
+        self.p1 = u'{0}*'.format(self.mp_host_port)
+
+        self.getcaps_url = \
+            '{0}/service?version=1.1.1&service=WMS&request=GetCapabilities'\
+            .format(self.mp_root.rstrip('/').lower())
+
+    def tearDown(self):
+        pass
+
+    def testSslContextAdapter(self):
+        config = self.ssl_config_4
+        self.create_hostname_port_mapping(config, self.p1)
+        self.assertEqual(HostnamePortSslConfig.objects.count(), 1)
+
+        ssla = SslContextAdapter(self.mp_root)
+
+        # SslConfig options should round-trip. This indicates a hostname port
+        # mapping match has occurred within adapter and that the returned
+        # config's context options are the same as a direct conversion of
+        # expected SslConfig
+        self.assertEqual(
+            ssla.get_ssl_context_opts(normalize_hostname(self.mp_root)),
+            ssla.ssl_config_to_context_opts(self.ssl_config_4))
+        # Same, but via dump-to-tuple method
+        self.assertEqual(
+            ssla.context_options(),
+            ssla.ssl_config_to_context_opts(config))
+
+        # Ensure adapter's Retry object matches passed-in settings
+        _, _, adptr_opts = ssla.ssl_config_to_context_opts(config)
+        self.assertEqual(ssla.max_retries.total, adptr_opts['retries'])
+        self.assertEqual(ssla.max_retries.redirect, adptr_opts['redirects'])
+
+        # Request does not normalize URL
+        req = Request(method='GET', url=self.mp_root)
+        self.assertEqual(req.url, self.mp_root)
+
+        # PreparedRequest does normalize URL
+        p_req = req.prepare()
+        self.assertEqual(p_req.url, normalize_hostname(self.mp_root))
+
+        # SslContextAdapter should always normalize the URL, because urllib3's
+        # SSL cert hostname matching is case-sensitive (and shouldn't be)
+        self.assertEqual(ssla._normalize_hostname(self.mp_root),
+                         normalize_hostname(self.mp_root))
+        self.assertEqual(ssla._normalize_hostname(self.mp_root),
+                         p_req.url)
+
+        # Sending solely via adapter (outside of session) should work for
+        # connections that do not require cookies, etc.
+        resp = ssla.send(p_req)
+        self.assertEqual(resp.status_code, 200)
 
 
 class TestHostnamePortSslConfig(PkiTestCase):
@@ -273,7 +344,7 @@ class TestHostnamePortSslConfig(PkiTestCase):
         # self.assertEqual(HostnamePortSslConfig.objects.count(), 0)
         # self.assertEqual(len(https_client.adapters), 1)
 
-    @pytest.mark.skip(reason="Because it can't auth to running exhcange")
+    @pytest.mark.skip(reason="Because it can't auth to running exchange")
     def testMapProxyRegistration(self):
         logger.debug("Service.objects:\n{0}"
                      .format(repr(Service.objects.all())))
