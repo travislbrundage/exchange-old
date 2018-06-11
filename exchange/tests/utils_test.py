17c6b00b0239c6fb783444ed32b4ed82a8dcaedb
travis <travislbrundage@gmail.com>
2018-06-11 11:42:22 -0700

    Utils tests fixed

--- exchange/tests/utils_test.py
+++ exchange/tests/utils_test.py
@@ -1,21 +1,31 @@
 from . import ExchangeTest
 import mock
 import json
-import pytest
+from django.conf import settings
 
 
 class DefaultMapConfigTest(ExchangeTest):
 
-    def mock_proxy_route():
+    def mock_proxy_route(*args):
         return True
 
-    @pytest.mark.skip(reason="The mock_proxy_route is not done correctly")
-    @mock.patch("exchange.pki.models.uses_proxy_route",
+    @mock.patch("geonode.utils.uses_proxy_route",
                 side_effect=mock_proxy_route)
     @mock.patch("geonode.services.serviceprocessors.base.settings",
                 autospec=True)
-    def test_proxy_base_map(self, mock_settings, mock_proxy_route):
-        mock_settings.PROXY_BASEMAP = True
+    def test_proxy_base_map(self, mock_service_settings, mock_proxy_route):
+        mock_service_settings.PROXY_BASEMAP = True
+        # TODO: Is there a way to mock this instead?
+        settings.MAP_BASELAYERS = [{
+            # We're mocking return value, but the url string needs to exist
+            "source": {"ptype": "gxp_olsource", "url": "exists"},
+            "type": "OpenLayers.Layer",
+            "args": ["No background"],
+            "name": "background",
+            "visibility": False,
+            "fixed": True,
+            "group":"background",
+        }]
         from geonode.utils import default_map_config
         map_config, base_layers = default_map_config(None)
         self.assertTrue(json.loads(base_layers[0].source_params)['use_proxy'])
